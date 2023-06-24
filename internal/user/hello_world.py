6f10ae4be49aac995a2300d682d66d93020f7e71
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2023-06-24 11:30:55 +0200

    Fixed serverless.yml so it adds cors policy

--- internal/user/hello_world.py
+++ internal/user/hello_world.py
@@ -1,6 +1,13 @@
+import json
+
 
 def hello_world(event, context):
     return {
         'statusCode': 200,
-        'body': "Hello world"
+        'body': json.dumps({"message": "Hello world"}),
+        'headers': {
+            "Access-Control-Allow-Origin": "http://localhost:4200",
+            "Access-Control-Allow-Headers": "Authorization",
+            "Access-Control-Allow-Credentials": True
+        }
     }
