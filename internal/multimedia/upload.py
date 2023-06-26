1f358d1e2b42a6b7df50f98996a04b30a34614a8
cuturic01 <miloscuturic8@gmail.com>
2023-06-26 17:38:11 +0200

    #13 Implemented album file upload and display

--- internal/multimedia/upload.py
+++ internal/multimedia/upload.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import os
 import traceback
 from decimal import Decimal
 
@@ -10,8 +11,12 @@ from datetime import datetime
 from internal.model.multimedia_metadata import MultimediaMetadata
 from internal.model.my_response import my_response
 
+album_content_table_name = os.environ['ALBUM_CONTENT_TABLE']
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('multimedia_metadata')
+
+album_content_table = dynamodb.Table(album_content_table_name)
 
 
 def upload_file(event, context):
@@ -30,12 +35,15 @@ def upload_file(event, context):
         file_name = body['name']
         size_in_kb = Decimal(body['size'])  # size in KB
         file_extension = body['extension']
+        album_id = body['albumId']
 
         file_content = base64.b64decode(base64_string)
 
+        multimedia_id = str(uuid.uuid4())
+
         # Create a MultimediaMetadata object
         multimedia = MultimediaMetadata(
-            id=str(uuid.uuid4()),
+            id=multimedia_id,
             name=file_name,
             type=file_extension,
             size_in_kb=size_in_kb,
@@ -47,6 +55,9 @@ def upload_file(event, context):
 
         # Write the metadata to the DynamoDB table
         table.put_item(Item=multimedia.to_dict())
+
+        if album_id != "":
+            add_to_album(multimedia_id, album_id)
 
         # Upload the file to the S3 bucket
         s3 = boto3.client('s3')
@@ -61,3 +72,11 @@ def upload_file(event, context):
         print("message", str(e))
         print("traceback", traceback.format_exc())
         return my_response(500, {"message": str(e), "tracebck": traceback.format_exc()})
+
+
+def add_to_album(multimedia_id, album_id):
+    album_content_table.put_item(Item={
+        "id": str(uuid.uuid4()),
+        "album": album_id,
+        "file": multimedia_id
+    })
