9dbf0248bf162bfc2506693a8cebc1467e62a6a0
Marko Janosevic <markojanosevicsm7@gmail.com>
2023-06-27 18:15:50 +0200

    #16 Get shared albums and share content

--- internal/sharing/get_shared_albums.py
+++ internal/sharing/get_shared_albums.py
@@ -1,2 +1,35 @@
+import os
+import traceback
+import boto3
+from boto3.dynamodb.conditions import Attr
+from internal.model.my_response import my_response
+
+share_table_name = os.environ['SHARE_TABLE_NAME']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(share_table_name)
+
+
 def get_shared_albums(event, context):
-    pass
+    try:
+        email = event['pathParameters']['email']
+
+        response = table.scan(
+            FilterExpression=Attr('viewer').eq(email) & Attr('contentType').eq('album')
+        )
+        albums_data = response['Items']
+        albums = []
+
+        for data in albums_data:
+            response = table.scan(
+                FilterExpression=Attr('id').eq(data['contentId']) & Attr('deleted').eq(False)
+            )
+            if not response['Items']:
+                continue
+            albums.append(response['Items'][0])
+
+        return my_response(200, albums)
+
+    except Exception as e:
+        print("error message", str(e))
+        print("traceback", traceback.format_exc())
+        return my_response(500, {"message": str(e), "tracebck": traceback.format_exc()})
